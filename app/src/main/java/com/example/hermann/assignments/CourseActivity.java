521b26286ef6f8de9696f0fa63c4a5fe1349b000
Hermann Villanger <hermannvillanger@gmail.com>
2017-07-02 19:32:12 +0200

    CourseActivity: Forwarding of courseId to addAssignment, displaying course name and code on xml page

--- app/src/main/java/com/example/hermann/assignments/CourseActivity.java
+++ app/src/main/java/com/example/hermann/assignments/CourseActivity.java
@@ -7,8 +7,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import helper.DatabaseHelper;
+import model.Course;
 
 /**
  * Created by Hermann on 01.07.2017.
@@ -20,20 +22,41 @@ public class CourseActivity extends Activity{
     DatabaseHelper databaseHelper;
     SQLiteDatabase sqLiteDatabase;
     Button addAssignment;
+    Integer courseId;
+    Course course;
+    TextView courseName, courseCode;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course);
         context = getBaseContext();
+        Intent intent = getIntent();
+        courseId = intent.getIntExtra("courseId",0);
+
+        databaseHelper = new DatabaseHelper(context);
+        sqLiteDatabase = databaseHelper.getReadableDatabase();
+        courseName = findViewById(R.id.courseName);
+        courseCode = findViewById(R.id.courseCode);
 
         addAssignment = (Button) findViewById(R.id.addAssignment);
         addAssignment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent direct = new Intent(CourseActivity.this, AddAssignmentActivity.class);
+                direct.putExtra("courseId",courseId);
                 startActivity(direct);
             }
         });
+        course = databaseHelper.getCourse(courseId, sqLiteDatabase);
+
+        courseName.setText(course.getName());
+        courseCode.setText(course.getCCode());
     }
 
+    @Override
+    protected void onDestroy() {
+        sqLiteDatabase.close();
+        databaseHelper.close();
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/example/hermann/assignments/OverviewActivity.java
+++ app/src/main/java/com/example/hermann/assignments/OverviewActivity.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
 import helper.DatabaseHelper;
+import model.Course;
 
 /**
  * Created by Hermann on 01.07.2017.
@@ -21,6 +24,8 @@ public class OverviewActivity extends Activity{
     SQLiteDatabase sqLiteDatabase;
     Button addCourse;
     Button toCourse;
+    List<Course> courseList;
+    Integer courseId;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -30,15 +35,8 @@ public class OverviewActivity extends Activity{
         databaseHelper = new DatabaseHelper(context);
         sqLiteDatabase = databaseHelper.getReadableDatabase();
 
+        courseList = databaseHelper.getAllCourses(sqLiteDatabase);
 
-        toCourse = (Button) findViewById(R.id.toCourse);
-        toCourse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent direct = new Intent(OverviewActivity.this, CourseActivity.class);
-                startActivity(direct);
-            }
-        });
         addCourse = (Button) findViewById(R.id.addCourse);
         addCourse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +46,25 @@ public class OverviewActivity extends Activity{
             }
         });
 
+        //TODO Knapp for her Course, som sender courseId videre
+        toCourse = (Button) findViewById(R.id.toCourse);
+        toCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent direct = new Intent(OverviewActivity.this, CourseActivity.class);
+                direct.putExtra("courseId", courseId);
+                startActivity(direct);
+            }
+        });
+
+
+        sqLiteDatabase.close();
     }
 
+    @Override
+    protected void onDestroy() {
+        sqLiteDatabase.close();
+        databaseHelper.close();
+        super.onDestroy();
+    }
 }
